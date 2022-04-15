4f59a23c9c8fd80fbcd0b3bb6d668a667d6d28fd
Remelyn Tampol <remelyn.tampol@gmail.com>
2022-04-15 02:01:10 -0700

    Index File changed

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Tune Up </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
@@ -31,7 +31,7 @@
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
                 <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <p class="lead"> Hi, this web page is consist of song lyrics</p>
                 
             </div>
         </header>
@@ -40,16 +40,62 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                        <h2 class="text-primary">Easy On Me</h2>
+                        <p class="lead">Adele</p>
+                        <?php
+
+
+                echo "<p> There ain't no gold in this river </p>";
+                echo "<p> That I've been washin' my hands in forever </p>";
+                echo "<p> I know there is hope in these waters </p>";
+                echo "<p> But I can't bring myself to swim </p>";
+                echo "<p> When I am drowning in this silence </p>";
+                echo "<p> Baby, let me in </p> <br>";
+
+                echo "<p> Go easy on me, baby </p>";
+                echo "<p> I was still a child </p>";
+                echo "<p> Didn't get the chance to </p>";
+                echo "<p> Feel the world around me </p>";
+                echo "<p> I had no time to choose </p>";
+                echo "<p> What I chose to do </p>";
+                echo "<p> So go easy on me </p> <br>";
+
+                echo "<p> There ain't no room for things to change </p>";
+                echo "<p> When we are both so deeply stuck in our ways </p>";
+                echo "<p> You can't deny how hard I have tried </p>";
+                echo "<p> I changed who I was to put you both first </p>";
+                echo "<p> But now I give up </p> <br>";
+
+                echo "<p> Go easy on me, baby </p>";
+                echo "<p> I was still a child </p>";
+                echo "<p> Didn't get the chance to </p>";
+                echo "<p> Feel the world around me </p>";
+                echo "<p> Had no time to choose </p>";
+                echo "<p> What I chose to do </p>";
+                echo "<p> So go easy on me </p> <br>";
+
+                echo "<p> I had good intentions </p>";
+                echo "<p> And the highest hopes </p>";
+                echo "<p> But I know right now </p>";
+                echo "<p> That probably doesn't even show </p> <br>";
+
+                echo "<p> Go easy on me, baby </p>";
+                echo "<p> I was still a child </p>";
+                echo "<p> I didn't get the chance to </p>";
+                echo "<p> Feel the world around me </p>";
+                echo "<p> I had no time to choose </p>";
+                echo "<p> What I chose to do </p>";
+                echo "<p> So go easy on me </center></p>";
+   
+    ?>
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Remelyn Zulueta Tampol</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
